0f22cea61fccdce48ee3e51277cf9a4f7c891e90
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-09-11 23:23:31 +0700

    bg

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-full  min-h-screen" :style="cssProps" >
+    <div class="w-full min-h-screen bg-gray-300 bg-opacity-50 ">
+        <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        cssProps: {
+          backgroundImage: `url(${require("../assets/img/bg.png")})`,
+          backgroundSize: "400px 400px",
+        }
+      };
+    },
+  }
+</script>
