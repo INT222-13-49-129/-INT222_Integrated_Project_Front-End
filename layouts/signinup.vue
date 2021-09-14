c19a89a5725c3648ea369d3c258aca9a076420bc
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-09-14 17:36:15 +0700

    [ADD} signin.vue

---
+++ layouts/signinup.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full  h-full fixed bg-gradient-to-br from-white via-lightsalmon to-salmon">
+    <div class="w-full  h-full fixed" :style="cssProps">
+    <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        cssProps: {
+          backgroundImage: `url(${require("../assets/img/bg2.svg")})`,
+        }
+      };
+    }
+  }
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,7 @@
 export default {
+  server: {
+    port: 5000 // default: 3000
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'cfan-int222',
