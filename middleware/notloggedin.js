8ec2b6733bc2c7e29864a5f34789bbc18c61accc
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-09-23 08:46:58 +0700

    [Update] middleware

--- middleware/notloggedin.js
+++ middleware/notloggedin.js
@@ -1,5 +1,5 @@
-export default function ({ store, redirect }) {
+export default function ({ store }) {
     if (store.state.auth.loggedIn) {
-      return redirect('/user')
+      this.$router.replace('/user')
     }
 }
