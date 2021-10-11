9c4ee844bb93998462edf0c45331a94e78c573e2
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-10-11 15:00:17 +0700

    [Update] default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full  h-full fixed" :style="cssProps" >
     <div class="w-full h-full bg-gray-300 bg-opacity-50 fixed" :class="{ 'xl:py-0 py-8 flex flex-col overflow-scroll': isindex }">
-        <div :class="isindex ? 'my-auto':'w-screen h-screen fixed overflow-scroll'">
+        <div :class="isindex ? 'my-auto':'w-full h-full fixed overflow-scroll'">
         <TheHeader :isindex="isindex"  :class="isindex ? 'xl:px-16 mx-auto xl:w-336 w-11/12 h-20 bg-white shadow-lg' : 'fixed top-0 xl:px-16 min-w-max w-full h-20 bg-white shadow-md z-10'"/>
         <Nuxt />
         </div>
