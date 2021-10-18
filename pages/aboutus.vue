b2ee97a430e44bc80fe86e287bd8a0cda73799c9
chuphom <priyanut.buadum.11.2.2544@gmail.com>
2021-10-19 02:10:20 +0700

    About us

--- pages/aboutus.vue
+++ pages/aboutus.vue
@@ -1,31 +1,74 @@
 <template>
-  <div class="mx-auto xl:w-336 w-11/12 h-auto pb-10 bg-gradient-to-b  from-white to-gray-200 shadow-lg">
-   <div class="flex xl:flex-row flex-col-reverse xl:px-32 justify-center">
-     <div class="xl:w-7/12 w-full xl:pl-12 flex flex-row h-36 justify-between ">
-       <img  src="../assets/img/member/chompoo.jpg" />
-       <img  src="../assets/img/member/chompoo.jpg" />
-       <img  src="../assets/img/member/chompoo.jpg" />
-     </div>
-   </div>
-   <div class="flex xl:flex-row flex-col justify-center w-3/4 mx-auto xl:gap-x-10 gap-y-8 mt-6 xl:mt-0 text-white text-lg">
-      <nuxt-link class="transition duration-300 ease-in-out transform hover:scale-110 group bg-lightsalmon hover:bg-salmon hover:shadow-md px-5 py-2 rounded-full flex justify-center" to="/foodmenu">ค้นหาแคลอรี่ของอาหาร 
-      <i class="material-icons text-xl ml-3">
-        search
-      </i></nuxt-link>
-      <nuxt-link class="transition duration-300 ease-in-out transform hover:scale-110 group bg-lightsalmon hover:bg-salmon hover:shadow-md px-5 py-2 rounded-full flex justify-center" to="/">คำนวนแคลอรี่ใหม่
-      <i class="material-icons text-xl ml-3">
-        add
-      </i></nuxt-link>
-      <nuxt-link class="transition duration-300 ease-in-out transform hover:scale-110 group bg-lightsalmon hover:bg-salmon hover:shadow-md px-5 py-2 rounded-full flex justify-center" to="/signup">สมัครสมาชิก
-      <i class="material-icons text-xl ml-3 group-hover:animate-bouncex">
-        east
-      </i></nuxt-link>
-   </div>
+  <div class="mx-auto xl:w-336 w-11/12 h-auto pb-10 bg-white shadow-lg">
+    <div>
+
+
+      <div class="flex xl:flex-row xl:mx-32 justify-center text-base">
+        <div class="w-1/2">
+          <p class="text-3xl text-salmon mt-40 ">CFAN </p>
+          <p class="text-salmon mt-5">The system calculates calories from food ingredients <br> and assesses primary
+            nutrition. </p>
+          <p class="text-gray-800 mt-5 text-right">ระบบคำนวณแคลอรี่<br>จากส่วนผสมในอาหารและประเมินภาวะโภชนาการเบื่องต้น
+          </p>
+          <div class="flex flex-row justify-end w-full mx-full mt-10 xl:gap-x-10 gap-y-8 mt-6 text-salmon text-lg">
+            <div
+              class="px-12 py-2 rounded-sm flex justify-center border border-lightsalmon text-sm my-auto transition duration-300 ease-in-out transform hover:scale-110 group bg-white hover:bg-salmon hover:shadow-2xl hover:text-white shadow-xl">
+              More
+            </div>
+            <div
+              class="px-12 py-2 rounded-sm flex justify-center border border-salmon text-sm text-white my-auto transition duration-300 ease-in-out transform hover:scale-110 group bg-lightsalmon hover:bg-salmon hover:shadow-2xl hover:text-white shadow-xl">
+              Member
+            </div>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <img class="w-11/12 mx-auto" src="../assets/img/member/bg/1.svg" />
+        </div>
+      </div>
+      <div class="flex xl:flex-row xl:mx-32 justify-center text-base mt-28">
+        <div class="w-1/2">
+          <img class="w-11/12 mx-auto" src="../assets/img/member/bg/2.svg" />
+        </div>
+        <div class="w-1/2">
+          <p class="text-3xl text-salmon mt-40 ">CFAN </p>
+          <p class="text-salmon mt-5">The system calculates calories from food ingredients <br> and assesses primary
+            nutrition. </p>
+          <div class="text-gray-800 mt-5 ">เป็นระบบเกี่ยวกับการจดบันทึกข้อมูลแคลอรี่จากการกินเฉพาะบุคคลในแต่ละวันซึ่ง
+            ฟังก์ชั่นหลักจะอยู่ที่การช่วยคำนวนแคลอรี่ของอาหารให้ใกล้เคียงกับอาหารจานของคุณมากที่สุด
+            โดยการถามถึงปริมาณวัตถุดิบในอาหารจานนั้นๆ และ บันทึกแคลอรี่ที่ได้รับในแต่ละวัน โดยจะมีฟังก์ชั่นรองอื่นๆ เช่น
+            ระบบช่วยประเมินความเหมาะสมของแคลอรี่ที่ควรได้รับในแต่ละวัน
+          </div>
+        </div>
+      </div>
+
+      <div class="flex xl:flex-row xl:mx-32 justify-center text-base text-gray-800 mt-40">
+        <div class="w-5/12">
+          <p class="text-3xl text-salmon ">MEMBER </p>
+          <p class="text-3xl mt-10">นางสาว ปรียานุช บัวดำ<br> 62130500129</p>
+          <p class="mt-10">หน้าที่ :<br>ออกแบบ UXUI<br>เขียน Front-End<br></p>
+        </div>
+        <div class="w-4/12">
+          <img class="w-11/12 my-auto mx-auto" src="../assets/img/member/bg/3.svg" />
+        </div>
+        <div class="w-3/12 mx-auto h-36 justify-between -mt-12 ">
+          <img class="w-32 mb-5 rounded-full shadow-xl transform hover:scale-110 transition duration-300 ease-in-out" src="../assets/img/member/chompoo.jpg" />
+          <img class="w-32 my-5 rounded-full shadow-xl transform hover:scale-110 transition duration-300 ease-in-out ml-16 " src="../assets/img/member/chompoo.jpg" />
+          <img class="w-32 my-5 rounded-full shadow-xl transform hover:scale-110 transition duration-300 ease-in-out" src="../assets/img/member/chompoo.jpg" />
+        </div>
+      </div>
+      <div class="text-right xl:mx-32 text-xs mt-20">
+        <p>INT222 Information Technology Integrated Project II </p>
+        <p>IT , SIT : King Mongkut’s University of Technology Thonburi</p>
+        <p>2021</p>
+      </div>
+    </div>
+
   </div>
 </template>
-<script>
-export default {
 
-}
+<script>
+  export default {
+
+  }
 
 </script>
