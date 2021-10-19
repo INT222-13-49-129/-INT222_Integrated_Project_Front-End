30682ec17c4133bdeafd49b66e742cd66648a1b7
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-10-20 05:44:10 +0700

    [Update] FoodmenuImg

--- components/FoodmenuImg.vue
+++ components/FoodmenuImg.vue
@@ -1,6 +1,6 @@
 <template>
     <div v-lazy-container="{ selector: 'img' }">
-        <img class="object-cover h-full" :data-src="img" data-loading="../assets/img/loading.svg" />
+        <img class="object-cover h-full" :data-src="img" :data-loading="require('../assets/img/preload.jpg')" />
     </div>
 </template>
 <script>
