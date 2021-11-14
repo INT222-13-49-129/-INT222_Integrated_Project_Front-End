6c03f6d15703eff21fd739d974c85f918f872340
Kamin124712T <kamin.124712@mail.kmutt.ac.th>
2021-11-14 20:58:29 +0700

    [Update] profile setting

--- layouts/user.vue
+++ layouts/user.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-gray-100 w-full h-full fixed overflow-scroll flex xl:flex-row flex-col">
         <Sidebar />
-        <div class="xl:ml-64 w-full">
+        <div class="xl:ml-64 w-full h-full">
             <Nuxt />
         </div>
     </div>
